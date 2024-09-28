2a8cfe0eed416c1462524525a9cfcaad08432e80
Aman Verma <amanksverma@gmail.com>
2024-09-28 19:54:36 +0530

    Add vercel.json for error path

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<SignIn />}/>
-          <Route path='*' element={<Error />}/>
+          <Route path='/error' element={<Error />}/>
 
         </Routes>
       </BrowserRouter>
